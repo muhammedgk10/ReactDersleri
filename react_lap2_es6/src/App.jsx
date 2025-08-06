fb0bd45443fa79a2863ed628d2acab86ce7d230e
Muhammed Gök <gokmuhammed13@gmail.com>
2025-08-06 07:23:29 +0300

    ilk projeler oluştu

---
+++ react_lap2_es6/src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <>
+    <h1>Muhammed</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react_lap2_es6/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    {/* <h1>Muhammed Gök</h1> */} 
+    {/* Ctrl + Ö Tuşu ile seçili alanı yorum satırı yapaLım. */}
+    </StrictMode>,
+)
